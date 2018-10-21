62cec46f156088dcc089607497c1ec4f2bd23ecc
Kitaez71 <kitaez7142@gmail.com>
2018-10-21 12:26:13 +0000

    repo manifest v.1

--- lineage_5507.mk
+++ lineage_5507.mk
@@ -1,6 +1,9 @@
 # Inherit device configuration
 $(call inherit-product, device/Prest/5507/5507.mk)
 
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/cm/config/common.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := 5507
 PRODUCT_NAME := lineage_5507
